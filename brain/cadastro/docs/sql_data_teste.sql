445f4a4d4c01be2b43d22544aeb8e61413c98b08
mateu <mateus.vidotti@idgeo.com.br>
2024-08-10 14:19:48 -0300

    sql data teste

---
+++ brain/cadastro/docs/sql_data_teste.sql
@@ -0,0 +1,130 @@
+{
+    "nome": "João da Silva",
+    "cpf": "333.196.070-57",
+    "fazendas": [
+        {
+            "nome": "Fazenda A",
+            "cidade": "São Paulo",
+            "estado": "SP",
+            "area_total": 100,
+            "area_vegetacao": 20,
+            "areas_agricultaveis": [
+                {"cultura": "Soja", "area_agricultavel": 50},
+                {"cultura": "Café", "area_agricultavel": 20}
+            ]
+        },
+        {
+            "nome": "Fazenda B",
+            "cidade": "Belo Horizonte",
+            "estado": "MG",
+            "area_total": 200,
+            "area_vegetacao": 50,
+            "areas_agricultaveis": [
+                {"cultura": "Algodão", "area_agricultavel": 100}
+            ]
+        }
+    ]
+}
+
+
+{
+    "nome": "Maria Oliveira",
+    "cpf": "772.187.550-11",
+    "fazendas": [
+        {
+            "nome": "Fazenda C",
+            "cidade": "Curitiba",
+            "estado": "PR",
+            "area_total": 150,
+            "area_vegetacao": 30,
+            "areas_agricultaveis": [
+                {"cultura": "Milho", "area_agricultavel": 60},
+                {"cultura": "Café", "area_agricultavel": 40}
+            ]
+        },
+        {
+            "nome": "Fazenda D",
+            "cidade": "Goiânia",
+            "estado": "GO",
+            "area_total": 300,
+            "area_vegetacao": 80,
+            "areas_agricultaveis": [
+                {"cultura": "Soja", "area_agricultavel": 150},
+                {"cultura": "Cana-de-açúcar", "area_agricultavel": 70}
+            ]
+        }
+    ]
+}
+
+
+{
+    "nome": "Carlos Souza",
+    "cpf": "142.376.040-98",
+    "fazendas": [
+        {
+            "nome": "Fazenda E",
+            "cidade": "Porto Alegre",
+            "estado": "RS",
+            "area_total": 250,
+            "area_vegetacao": 50,
+            "areas_agricultaveis": [
+                {"cultura": "Café", "area_agricultavel": 100},
+                {"cultura": "Algodão", "area_agricultavel": 70}
+            ]
+        },
+        {
+            "nome": "Fazenda F",
+            "cidade": "Ribeirão Preto",
+            "estado": "SP",
+            "area_total": 350,
+            "area_vegetacao": 120,
+            "areas_agricultaveis": [
+                {"cultura": "Milho", "area_agricultavel": 150},
+                {"cultura": "Soja", "area_agricultavel": 80}
+            ]
+        }
+    ]
+}
+
+{
+    "nome": "José Freitas",
+    "cpf": "088.628.380-93",
+    "fazendas": [
+        {
+            "nome": "Fazenda G",
+            "cidade": "Goiânia",
+            "estado": "GO",
+            "area_total": 500,
+            "area_vegetacao": 150,
+            "areas_agricultaveis": [
+                {"cultura": "Soja", "area_agricultavel": 200},
+                {"cultura": "Milho", "area_agricultavel": 100},
+                {"cultura": "Café", "area_agricultavel": 50}
+            ]
+        },
+        {
+            "nome": "Fazenda H",
+            "cidade": "Curitiba",
+            "estado": "PR",
+            "area_total": 450,
+            "area_vegetacao": 100,
+            "areas_agricultaveis": [
+                {"cultura": "Cana-de-açúcar", "area_agricultavel": 150},
+                {"cultura": "Algodão", "area_agricultavel": 150},
+                {"cultura": "Soja", "area_agricultavel": 50}
+            ]
+        },
+        {
+            "nome": "Fazenda I",
+            "cidade": "Campo Grande",
+            "estado": "MS",
+            "area_total": 600,
+            "area_vegetacao": 200,
+            "areas_agricultaveis": [
+                {"cultura": "Milho", "area_agricultavel": 250},
+                {"cultura": "Soja", "area_agricultavel": 100},
+                {"cultura": "Algodão", "area_agricultavel": 50}
+            ]
+        }
+    ]
+}
